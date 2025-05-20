90353b7e747261079811b5b1ff65ed7996c10208
phamkhoa2505 <phamkhoa12332112@gmail.com>
2025-05-21 03:45:00 +0700

    first

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -13,7 +13,7 @@ import {
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: 3306,
+      port: 3307,
       username: 'root',
       password: '',
       database: 'health',
