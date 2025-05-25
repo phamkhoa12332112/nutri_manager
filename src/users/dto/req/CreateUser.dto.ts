b795e430f053c6b4abcd2c88151cc3b5acc17a17
banhTheCake <binhanh080103@gmail.com>
2025-05-25 09:37:55 +0700

    Impl method register/user

--- src/users/dto/req/CreateUser.dto.ts
+++ src/users/dto/req/CreateUser.dto.ts
@@ -1,32 +1,6 @@
-import {
-  IsBoolean,
-  IsNumber,
-  IsPositive,
-  IsString,
-  MinLength,
-} from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
-  @MinLength(3)
-  name: string;
-
-  @IsNumber()
-  @IsPositive()
-  age: number;
-
-  @IsBoolean()
-  gender: boolean;
-
-  @IsNumber()
-  weight: number;
-
-  @IsNumber()
-  height: number;
-
-  @IsNumber()
-  dailyCaloriesGoal: number;
-
   @IsString()
   @MinLength(3)
   email: string;
